5c968160d0c70bb36a01438533b7797534bea3d5
Jamário Nascimento <jamariobatista@gmail.com>
2024-03-06 12:46:03 -0300

    configurando recuperação de senha

---
+++ app/Http/Controllers/TarefaController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Tarefa;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TarefaController extends Controller
+{
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+
+    public function index()
+    {
+
+        // $id = auth()->user()->id;
+        // $nome = auth()->user()->name;
+        // $email = auth()->user()->email;
+        // return 'Nome: '.$nome.' Email: '.$email.' Id: '.$id.'Você está logado';
+
+        if(Auth::check()){
+            $id =Auth::user()->id;
+            $nome =Auth::user()->name;
+            $email = Auth::user()->email;
+            return 'Nome: '.$nome.' Email: '.$email.' Id: '.$id.'Você está logado';
+        }else{
+            return 'Não está logado';
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Tarefa $tarefa)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Tarefa  $tarefa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Tarefa $tarefa)
+    {
+        //
+    }
+}
