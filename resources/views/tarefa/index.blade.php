ef6cb8e4a38d6a46baf31e13af5944cb3aaf9a5c
Jamário Nascimento <jamariobatista@gmail.com>
2024-03-09 16:28:35 -0300

    Add novo menu topo.

--- resources/views/tarefa/index.blade.php
+++ resources/views/tarefa/index.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Tarefas</div>
+                    <div class="card-header">Tarefas <a href="{{ route('tarefa.create') }}" class="btn btn-primary float-end">Adicionar Tarefa</a></div>
 
                     <div class="card-body">
                         <table class="table">
